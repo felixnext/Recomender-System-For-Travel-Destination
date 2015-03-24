dca661aa2cf9a6cd78220fde38892ec6590d87c3
yevgen <y.pikus@tu-bs.de>
2015-03-24 10:35:53 +0100

    Lsi similarity implemented

--- src/tools/python/LsiSimilarity.py
+++ src/tools/python/LsiSimilarity.py
@@ -10,28 +10,54 @@ class LsiSimilarity:
 
     stop_words = get_stop_words('en')
 
-    def getLsiSimilarity(self, docs, vectors):
+    def getLsiSimilarity(self, docs1, docs2):
 
         tokenizer = RegexpTokenizer(r'\w+')
         #delete stopwords & tokenize
-        docs = [[word for word in tokenizer.tokenize(doc.lower()) if word not in self.stop_words] for doc in docs]
-        vectors = [[word for word in tokenizer.tokenize(doc.lower()) if word not in self.stop_words] for doc in vectors]
+        docs1 = [[PorterStemmer().stem_word(word) for word in tokenizer.tokenize(doc.lower())
+        if word not in self.stop_words] for doc in docs1]
 
-        all_tokens = sum(docs, [])
+        docs2 = [[PorterStemmer().stem_word(word) for word in tokenizer.tokenize(doc.lower())
+        if word not in self.stop_words] for doc in docs2]
 
-        print all_tokens
-        print vectors
-
-        #stemming
-
-        print self.stop_words.__contains__(vectors)
-
-        #stem words
+        all_tokens = sum(docs1, [])
 
         #transform into vecotra
+        #creates dictionary on doc1
+        dictionary = corpora.Dictionary([all_tokens])
 
-        #compare
+        #create corpus
+        corpus = [dictionary.doc2bow(text) for text in docs1]
+        print corpus
+
+        #transform corpora into lsi space
+        lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+
+        #convert doc2 to vectors in doc1 space
+        docs2_vectors = [dictionary.doc2bow(doc) for doc in docs2]
+        docs2_lsi = [lsi[vec_bow] for vec_bow in docs2_vectors]
+        #print docs2_vectors
+
+        # transform corpus to LSI space and index it
+        index = similarities.MatrixSimilarity(lsi[corpus])
+
+        #claculate similarities
+        sims = [index[vec_lsi]  for vec_lsi in  docs2_lsi]
+
+        max_similarities = [max(sim) for sim in sims]
+
+        return max_similarities
 
 
 sim = LsiSimilarity()
-sim.getLsiSimilarity(["a a blam, ba","b"],["a","you are Bla.","blabla"])
+documents = ["Human machine interface for lab abc computer applications",
+              "A survey of user opinion of computer system response time",
+              "The EPS user interface management system",
+              "System and human system engineering testing of EPS",
+              "Relation of user perceived response time to error measurement",
+              "The generation of random binary unordered trees",
+              "The intersection graph of paths in trees",
+              "Graph minors IV Widths of trees and well quasi ordering",
+              "Graph minors A survey"]
+
+print sim.getLsiSimilarity(documents,["computer auto controling","Human computer interaction"])
