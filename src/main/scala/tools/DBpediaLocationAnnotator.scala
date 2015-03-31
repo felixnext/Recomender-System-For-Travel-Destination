a0e299c934e87e024cd4733ec3da7159ffce6295
yevgen <y.pikus@tu-bs.de>
2015-03-31 11:54:02 +0200

    implemntation of xml writer

--- src/main/scala/tools/DBpediaLocationAnnotator.scala
+++ src/main/scala/tools/DBpediaLocationAnnotator.scala
@@ -3,6 +3,9 @@ package tools
 import com.google.gson.{Gson, JsonElement}
 import dbpedia.{DBPediaClient, SpotlightClient}
 
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
 /**
  * This class takes a location name and annotate the location with dbpedia data.
  */
@@ -17,36 +20,59 @@ object DBpediaLocationAnnotator extends App {
   }
 
   //annotates location with dbpedia data
-  def annotateLocation(locationName: String ) = {
-
+  def annotateLocation(locationName: String): Option[Map[String, Set[String]]] = {
     val dbpedia = new DBPediaClient()
-
+    var annotations: Option[Map[String, Set[String]]] = None
     try {
-      println (dbpedia.parseDBpediaPageOfLocation(annotationSpotlight(locationName)._2.toString.replaceAll("\"", "")))
+      annotations = Some(dbpedia.parseDBpediaPageOfLocation(annotationSpotlight(locationName)._2.toString.replaceAll("\"", "")))
     } catch {
       case e: NullPointerException => {
         println("No spotlight results found for: " + locationName)
         //TODO maybe multiple uris
         val result = dbpedia.findDBpediaLocation(locationName)
-        if(!result.isEmpty) dbpedia.parseDBpediaPageOfLocation(result.head)
+        if (!result.isEmpty) annotations = Some(dbpedia.parseDBpediaPageOfLocation(result.head))
         else println("No dbpedia results found for: " + locationName)
       }
     }
-
+    annotations
   }
 
-  annotateLocation("bla")
-  annotateLocation("Berlin")
-  annotateLocation("Peguera")
-  annotateLocation("Germany")
-  annotateLocation("Antalya")
-  annotateLocation("Hannover")
+  //############
+  //Launch App
+  //############
+  if (args.length != 2) {
+    println("Excpetion: Cannot read parameters \n Format: dump_type{wikipedia, trevelerswiki, virtualpoint} path ")
+  }
+  else {
+    val dump_type = args(0)
+    val path = args(2)
 
 
-  //TODO paras params path and wiki
-  //dummy
-  val path = ""
-  val dump: DumpXMLReader = new TravelerPointReader(path)
+    val xml: DumpXMLReader = dump_type match {
+      case "wikipedia" => new Wikipedia(path)
+      case "trevelerswiki" => new Travelerswiki(path)
+      case "virtualpoint" => new TravelerPoint(path)
+      case _ => println("ERROR: Dump source dosn't match any of known sources"); null
+    }
+
+    while (xml.hasMorePages) {
+      Future {
+        var page: Map[String, Map[String, Set[String]]] = xml.readPage
+        //assumption: title is not empty
+        val title = page.getOrElse("title", Map("none" -> "none")).keySet.head
+
+        annotateLocation(title) match {
+          case Some(annotation) => page += ("dbpedia" -> annotation)
+          case None => page += ("dbpedia" -> Map("" -> Set("")))
+        }
+
+        xml.writePage(page)
+      }
+    }
+
+
+
+  }
 
 
 }
--- src/main/scala/tools/DumpXMLReader.scala
+++ src/main/scala/tools/DumpXMLReader.scala
@@ -1,31 +1,97 @@
 package tools
 
+import scalax.io.{Output, Resource, Codec}
+
+
 /**
  * Allows to read and write dumps in xml format
  */
-trait DumpXMLReader {
+abstract class DumpXMLReader(var path: String) {
+
+  val output = {
+    val output:Output = Resource.fromFile(path.substring(0,path.length-4)+"_annotated.xml")
+    output.write("<pages>")(Codec.UTF8)
+    output
+  }
 
   //Map(title -> Map(mytitle -> mytitle), text -> Map(p1 -> contetn, p2 -> content,..),lat -> Map(lat->lat), long -> ..)
-  def readPage: Map[String, Map[String,String]]
+  def readPage: Map[String, Map[String, Set[String]]]
 
-  def writePage(page: Map[String, Map[String,String]])
+  def hasMorePages: Boolean
+
+  def writePage(page: Map[String, Map[String, Set[String]]]) = {
+    output.write("  <page>")
+
+
+    output.write("  </page>")
+
+  }
+
+  def close() = {
+    output.write("</pages>")
+  }
 
 }
 
-class TravelerPointReader(path: String) extends DumpXMLReader{
-  override def readPage: Map[String, Map[String,String]] = ???
+class TravelerPoint(path: String) extends DumpXMLReader(path) {
 
-  override def writePage(page: Map[String, Map[String,String]]): Unit = ???
+
+  val source = scala.io.Source.fromFile(path)
+
+  var pages: List[Map[String, Map[String, Set[String]]]] = List()
+
+  var title = ""
+  var paragraphs: Map[String, Set[String]] = Map()
+  var paragraphsName = ""
+
+  for (line <- source.getLines()) {
+    if (line.contains("<place>")) {
+      title = line.replace("<place>", "").replace("<\\place>", "")
+    }
+    if (line.contains("<\\description>")) {
+      pages = pages :+ Map(title -> paragraphs)
+      title = ""
+      paragraphs = Map()
+      paragraphsName = ""
+    }
+    if (line.contains("<paragraph name")) {
+      paragraphsName = line.replace( """<paragraph name="""", "").replace("\">", "")
+    }
+    if (line.contains("<p>") && !line.contains("All Rights Reserved Utrecht")) {
+      val text = line.substring(3, line.length - 4)
+      val set: Set[String] = paragraphs.getOrElse(paragraphsName,Set()) + text
+      paragraphs += (paragraphsName -> set)
+    }
+
+  }
+
+  override def readPage: Map[String, Map[String, Set[String]]] = {
+    pages.synchronized {
+      val page = pages.head
+      pages = pages.tail
+      page
+    }
+  }
+
+  override def writePage(page: Map[String, Map[String, Set[String]]]): Unit = {
+
+  }
+
+  override def hasMorePages: Boolean = pages.isEmpty
 }
 
-class Wikipedia(path: String) extends DumpXMLReader {
-  override def readPage: Map[String, Map[String,String]] = ???
+class Wikipedia(path: String) extends DumpXMLReader(path) {
+  override def readPage: Map[String, Map[String, Set[String]]] = ???
 
-  override def writePage(page: Map[String, Map[String,String]]): Unit = ???
+  override def writePage(page: Map[String, Map[String, Set[String]]]): Unit = ???
+
+  override def hasMorePages: Boolean = ???
 }
 
-class Travelerswiki(path: String) extends DumpXMLReader {
-  override def readPage: Map[String, Map[String,String]] = ???
+class Travelerswiki(path: String) extends DumpXMLReader(path) {
+  override def readPage: Map[String, Map[String, Set[String]]] = ???
 
-  override def writePage(page: Map[String, Map[String,String]]): Unit = ???
+  override def writePage(page: Map[String, Map[String, Set[String]]]): Unit = ???
+
+  override def hasMorePages: Boolean = ???
 }
