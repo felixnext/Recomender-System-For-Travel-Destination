b019321602ac70a3e192ef64c7de7c99a300afb5
yevgen <y.pikus@tu-bs.de>
2015-04-04 11:03:53 +0200

    new elasticsearch commands added

--- start_script.sh
+++ start_script.sh
@@ -8,3 +8,16 @@ nohup java -jar -Xmx30G Destination-Recomender-System-assembly-0.1.jar wikipedia
 
 #starts travelerwiki dump annotation
 nohup java -jar -Xmx30G Destination-Recomender-System-assembly-0.1.jar trevelerswiki ./wikitravelorg_wiki_en-20110605-current.xml &
+
+######################
+#elasticsearch
+#######################
+
+#start elasticsearch
+bin/elasticsearch -d -Xmx5g -Xms5g -Des.index.store.type=memory --Des.max-open-files=64000 --node.name=RS1
+
+#bulk load data
+curl -XPOST 'localhost:9200/_bulk?pretty' --data-binary @file.json
+
+#stop elasticsearch
+curl -XPOST 'http://localhost:9200/_shutdown'
