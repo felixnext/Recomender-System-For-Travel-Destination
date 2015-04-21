84781c680131d2a407c0c25b960f73902ed28c56
yevgen <y.pikus@tu-bs.de>
2015-04-21 12:32:56 +0200

    join method for equal nodes implemented

--- src/main/scala/core/SparqlQueryCreator.scala
+++ src/main/scala/core/SparqlQueryCreator.scala
@@ -5,10 +5,9 @@ import elasticsearch.ElasticsearchClient
 import nlp._
 
 import scala.annotation.tailrec
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.math._
 
 
@@ -29,7 +28,7 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
 
     //split each word in sentece on "/". This converts words form word/pos into tuple (word,pos)
     //TODO change to method
-    val tokenizedSentensesPos: Future[Array[Array[(String, String)]]] = annotatedText.map{ x =>
+    val tokenizedSentensesPos: Future[Array[Array[(String, String)]]] = annotatedText.map { x =>
       x.stanford.sentencesPos.map { x =>
         x.split(" ").map { x =>
           val split = x.split("/")
@@ -68,10 +67,10 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
     for (e <- entityCandidatesAnnotation.failed) println("Candidate set creation failed. Cannot create sparql query" + e)
 
     //maps raw relations into patty dbpedia predicates
-    val predicateAnnotation = entityCandidatesAnnotation.map {c =>
+    val predicateAnnotation = entityCandidatesAnnotation.map { c =>
       val rel = c.groupsMap.map(k => (k._1,
-        k._2.map(r => new AnnontatedRelation(r.arg1,r.rel,r.relOffset,r.arg2,
-        Some(elastic.findPattyRelation(r.rel._2)), Some(elastic.findDBPediaProperties(r.rel._1))))))
+        k._2.map(r => new AnnontatedRelation(r.arg1, r.rel, r.relOffset, r.arg2,
+          Some(elastic.findPattyRelation(r.rel._2)), Some(elastic.findDBPediaProperties(r.rel._1))))))
       val smallerSets = rel.filter(x => x._2.size < 2).flatMap(x => x._2)
       val biggerSets = rel.filterNot(x => x._2.size < 2)
       val newUnkownGroup = biggerSets.getOrElse(-1, Seq()) ++ smallerSets
@@ -85,19 +84,19 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
       def annRelation(r: AnnontatedRelation) = {
         val arg1Lemon = elastic.findDBPediaClasses(r.arg1.arg)
         val arg1Yago = YagoGeoTypes.getYagoEntities(r.arg1.arg)
-        val arg1 = new AnnotatedArgument(r.arg1.spotlight,r.arg1.clavin, r.arg1.dbpediaLookup, r.arg1.arg, r.arg1.argType,
+        val arg1 = new AnnotatedArgument(r.arg1.spotlight, r.arg1.clavin, r.arg1.dbpediaLookup, r.arg1.arg, r.arg1.argType,
           r.arg1.argOffset, Some(arg1Yago), Some(arg1Lemon))
-        val args2 = for(argS <- r.arg2) yield {
+        val args2 = for (argS <- r.arg2) yield {
           val arg2Lemon = elastic.findDBPediaClasses(argS.arg)
           val arg2Yago = YagoGeoTypes.getYagoEntities(argS.arg)
-          new AnnotatedArgument(argS.spotlight,argS.clavin, argS.dbpediaLookup, argS.arg, argS.argType,
+          new AnnotatedArgument(argS.spotlight, argS.clavin, argS.dbpediaLookup, argS.arg, argS.argType,
             argS.argOffset, Some(arg2Yago), Some(arg2Lemon))
         }
         new AnnontatedRelation(arg1, r.rel, r.relOffset, args2, r.pattyResult, r.dbpediaProps)
       }
 
       //annotates all entities with dbpedia classes and  yago geo types
-      //if annotation doesn't exists then the list attribute remain empty
+      //if annotation doesn't exists then the attribute remains empty list
       val annRelations = relations.map(x => (x._1, x._2.map(y => annRelation(y))))
 
       //TODO try to create new groups in unknown group
@@ -108,23 +107,44 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
       val subGroups = scala.collection.mutable.Map[String, Seq[AnnontatedRelation]]()
 
       @tailrec
-      def joinEqualNodes(relations: Seq[AnnontatedRelation], trees: Seq[Tree] = Seq()): Seq[Tree] = {
-        val r = relations.head
-        val firstRelationSubject = r.arg1
-
-        val newTree = for(tree <- trees) yield {
-          //compare rel and r
-          val edges = tree.edges
-          for(edge <- edges ) {
-            val secondRelationSubject = edge._1.arg1
-          }
-          //use score !!!
-          new Tree(null)
+      def joinEqualNodes(relations: Seq[AnnontatedRelation], trees: Seq[Tree] = Seq(),
+                         focusWordsTree: Option[Map[AnnontatedRelation, Weight]] = None): Seq[Tree] = {
+        if (relations.isEmpty) {
+            trees ++ (if(focusWordsTree.isDefined) Seq(new Tree(focusWordsTree.getOrElse(Map()))) else Nil)
         }
+        else {
+          val r = relations.head
+          val arg = r.arg1
+          var matchFound = false
 
-        //TODO claculate relation score
-        //TODO only if not matched then add new root
-        joinEqualNodes(relations.tail, newTree :+ new Tree(Map(r -> new Weight(relationScorer.calculateRelationScore(r),1.0))))
+          val newTrees = for (tree <- trees) yield {
+            //compare two relation and calculate best match score w.r.t used support source
+            val edges = tree.edges.foldLeft(Seq[(Boolean, Double)]()) {
+              (s, edge) =>
+                val comparison = compareArguments(edge._1.arg1, arg)
+                if (comparison._1) s :+ comparison else s
+            }
+
+            val bestMatch = edges.foldLeft((false, 0.0))((g, e) => if (e._1 && e._2 > g._2) e else g)
+
+            //if there are a match then add new relation to actual tree node
+            if (bestMatch._1) {
+              matchFound = true
+              new Tree(tree.edges + (r -> new Weight(relationScorer.calculateRelationScore(r), bestMatch._2)))
+            }
+            else tree
+          }
+
+          val newFocusWordsTree = if (focusWords.find(word => arg.arg.contains(word)).size > 0)
+            Some(focusWordsTree.getOrElse(Map()) + (r -> new Weight(relationScorer.calculateRelationScore(r), 0.5)))
+          else focusWordsTree
+
+
+          val newRoot = if (matchFound) newTrees
+          else newTrees :+ new Tree(Map(r -> new Weight(relationScorer.calculateRelationScore(r), 1.0)))
+
+          joinEqualNodes(relations.tail, newRoot, newFocusWordsTree)
+        }
       }
 
       //compares two arguments and returns boolean meaning there are some equal mentions
@@ -146,13 +166,17 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
           case (Some(x), Some(xy)) => (x.intersect(xy).nonEmpty, 1.0)
           case _ => (false, 0.0)
         }
-        val d = (arg1.dbpediaLookup,arg2.dbpediaLookup) match {
-          case (Some(x), Some(xy)) => {for(a <- x; b <- xy if a.uri.equals(b.uri))
-            yield { (true, a.score + b.score - (a.score * b.score)) }}
-            .foldLeft((false, 0.0))((t, r) => if(r._2 > t._2) r else t)
+        val d = (arg1.dbpediaLookup, arg2.dbpediaLookup) match {
+          case (Some(x), Some(xy)) => {
+            for (a <- x; b <- xy if a.uri.equals(b.uri))
+              yield {
+                (true, a.score + b.score - (a.score * b.score))
+              }
+          }
+            .foldLeft((false, 0.0))((t, r) => if (r._2 > t._2) r else t)
           case _ => (false, 0.0)
         }
-        (s._1 || c._1 || l._1 || y._1 || d._1, Vector(s._2,c._2,l._2,y._2,d._2).foldLeft(0.0)((g,n) => max(g,n)))
+        (s._1 || c._1 || l._1 || y._1 || d._1, Vector(s._2, c._2, l._2, y._2, d._2).foldLeft(0.0)((g, n) => max(g, n)))
       }
 
       def chainRelations() = ???
@@ -161,9 +185,9 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
     }
 
 
-    Await.result(predicateAnnotation, 1000 seconds).foreach{
+    Await.result(predicateAnnotation, 1000 seconds).foreach {
       x => println("\n\n" + x._1)
-        //x._2.foreach(x => println(x.arg1.arg + " " + x.rel._1 + " " + x.arg2.map(y => y.arg).toString))
+      //x._2.foreach(x => println(x.arg1.arg + " " + x.rel._1 + " " + x.arg2.map(y => y.arg).toString))
     }
 
 
@@ -197,11 +221,11 @@ class SparqlQueryCreator extends TextAnalyzerPipeline {
       val posTaggedRelationList: Iterator[Array[String]] = for (subSentence <- slidingOverSentence
                                                                 if subSentence.map(t => t._1).sameElements(relationWords))
         yield {
-          subSentence.map(t =>  mapToPattyTags.getOrElse(t._2, t._1))
+          subSentence.map(t => mapToPattyTags.getOrElse(t._2, t._1))
         }
 
       val relTagged: String = if (posTaggedRelationList.isEmpty) relation.rel._1
-      else posTaggedRelationList.next().reduce((a,b) => a + " " + b)
+      else posTaggedRelationList.next().reduce((a, b) => a + " " + b)
 
       new Relation(relation.arg1, (relation.rel._1, relTagged), relation.relOffset, relation.arg2)
     }
@@ -244,13 +268,13 @@ class NaiveScorer extends RelationScorer {
       (spotlight + clavin + dbpediaYago + lookup) / 4.0
     }
 
-    val relationScore = ((if(r.dbpediaProps.isDefined) 1.0 else 0.0) + (if(r.pattyResult.isDefined) 1.0 else 0.0)) / 2.0
+    val relationScore = ((if (r.dbpediaProps.isDefined) 1.0 else 0.0) + (if (r.pattyResult.isDefined) 1.0 else 0.0)) / 2.0
 
     val arg1Score = calculateArgScore(r.arg1)
 
-    val maxArg2Score = r.arg2.foldLeft(0.0){(score, arg) =>
+    val maxArg2Score = r.arg2.foldLeft(0.0) { (score, arg) =>
       val actualScore = calculateArgScore(arg)
-      if(actualScore > score) actualScore else score
+      if (actualScore > score) actualScore else score
     }
 
     (relationScore + arg1Score + maxArg2Score) / 3.0
@@ -259,6 +283,6 @@ class NaiveScorer extends RelationScorer {
 
 //the double value of the map represents the membership score of relation
 //TODO Vector??
-case class Tree(edges: Map[AnnontatedRelation, Weight], children: Option[Map[AnnontatedRelation,Seq[Tree]]] = None)
+case class Tree(edges: Map[AnnontatedRelation, Weight], children: Option[Map[AnnontatedRelation, Seq[Tree]]] = None)
 
 case class Weight(weight: Double, factor: Double)
